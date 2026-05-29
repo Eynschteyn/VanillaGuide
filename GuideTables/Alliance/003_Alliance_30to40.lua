24d292f253e57ff9ac74d08671ea83aeea2bcdab
Eynschteyn <33424435+Eynschteyn@users.noreply.github.com>
2026-05-29 14:23:40 +0700

    Update 003_Alliance_30to40.lua

--- GuideTables/Alliance/003_Alliance_30to40.lua
+++ GuideTables/Alliance/003_Alliance_30to40.lua
@@ -207,9 +207,9 @@ Table_003_Alliance_30to40 = {
 			[34] = { str = "34. #HUNTERОбойдите кладбище кадо и приручите скорпиона 34 уровня и выучите навык пета claw rank 5. Скорпионы ходят в точке 37,80#", x = 37, y = 80, zone = "Desolace" },
 			[35] = { str = "35. Сейчас до 35 уровня должно остаться около 2х делений. Можно гриндить кентавров в точке 37,80 до 35 уровня", x = 37, y = 80, zone = "Desolace" },
 			[36] = { str = "36. Двигайтесь в Feralas и откройте полетчика, это сократит время в двльнейшем." },
-			[37] = { str = "37. В Feralas идите на корабль в точке 43,42 плывите на остров для открытия полетчика. Летите в Rachet.", x = 43, y = 42, zone = "Desolace" },
-			[38] = { str = "38. Зайдите к гоблину в точке 62,36 и сдайте задание  #TURNINGoblin Sponsorship pt.1#  возьмите следующее  #ACCEPTGoblin Sponsorship pt.2#.  Затем сдайте задание на пристане  #TURNINWharfmaster Dizzywig#  и берите следующее  #ACCEPTParts for Kravel#", x = 62, y = 36, zone = "Desolace" },
-			[39] = { str = "39. Идите на корабль и отправляйтесь в Booty Bay" },
+			[37] = { str = "37. В Feralas'е идите на корабль в точке 43,42 плывите на остров для открытия полетчика.", x = 43, y = 42, zone = "Desolace" },
+			[38] = { str = "38. Летите в Rachet. Сядьте на корабль и плывите в Booty Bay сдайте задание  #TURNINGoblin Sponsorship pt.2#  возьмите следующее  #ACCEPTGoblin Sponsorship pt.3#.  Затем сдайте задание на пристане  #TURNINWharfmaster Dizzywig#  и возьмите  #ACCEPTParts for Kravel#", x = 62, y = 36, zone = "Desolace" },
+			[39] = { str = "39. ." },
 		}
 	},
 
