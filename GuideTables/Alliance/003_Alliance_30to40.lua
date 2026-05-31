d773751cb57c70cbeea00767145e92dd29afc6c4
Eynschteyn <33424435+Eynschteyn@users.noreply.github.com>
2026-05-31 21:50:50 +0700

    Update 003_Alliance_30to40.lua

--- GuideTables/Alliance/003_Alliance_30to40.lua
+++ GuideTables/Alliance/003_Alliance_30to40.lua
@@ -257,7 +257,7 @@ Table_003_Alliance_30to40 = {
 			[34] = { str = "34. На данном этапе вы на половине пути к 37 уровню" },
 			[35] = { str = "35. Летите в Айронфордж, если вы ДРУИД летите в Штормград изучите новые навыки и летите в Menethil Harbor, привяжите там Херстоун, и плывите на коробле в Theramore" },
 			[36] = { str = "36. Если навык First aid на отметке 225 выполните задание  #ACCEPTTriage#  Летите в Gadgetzan (Танарис)" },
-			[37] = { str = "37. Бегите на север в соседнюю локаци Shimmering Flats в точку 77,77 и сдайте задание  #TURNINParts for Kravel#  берите следующее  #ACCEPTDelivery to the Gnomes#  затем сдайте его его.", x = 77, y = 77, zone = "Stranglethorn Vale" },
+			[37] = { str = "37. Бегите на север в соседнюю локацию Shimmering Flats в точку 77,77 и сдайте задание  #TURNINParts for Kravel#  берите следующее  #ACCEPTDelivery to the Gnomes#  затем сдайте его его.", x = 77, y = 77, zone = "Stranglethorn Vale" },
 			[38] = { str = "38. Идите на восток и сдайте квест  #TURNINGoblin Sponsorship pt.5#  и примите следующее  #ACCEPTThe Eighteenth Pilot#  сдайте его рядом и возьмите следующее  #ACCEPTRazzeric's Tweaking#" },
 			[39] = { str = "39. Возьмите задание  #ACCEPTThe Rumormonger#  в точке 77,77 и используйте Херстоун в Menethil Harbor", x = 77, y = 77, zone = "Stranglethorn Vale" },
 		}
